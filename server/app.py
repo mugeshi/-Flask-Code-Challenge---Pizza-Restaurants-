f0e9274e0c60c035f4b124dff3e04ff392b79eb0
mugeshi <nicolemugeshi@gmail.com>
2023-09-27 10:31:26 +0300

    added gunicorn

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 from models import db, Restaurant, Pizza, RestaurantPizza
 from flask_migrate import Migrate
@@ -6,6 +7,7 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pizza_restaurants.db'
+#postgres://pizza_restaurant_db_user:OIu88FKAsdK5KKqM2NBGFpjIdjOp6vhF@dpg-ck9ss7ddrqvc738o1710-a.oregon-postgres.render.com/pizza_restaurant_db
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
