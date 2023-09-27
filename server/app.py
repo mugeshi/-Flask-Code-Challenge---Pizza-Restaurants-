e2a8caaf185332d58a5ed191e3da3cc8152a63d5
Njeri Mugeshi <135008653+mugeshi@users.noreply.github.com>
2023-09-27 11:54:37 +0300

    Update app.py

--- server/app.py
+++ server/app.py
@@ -6,7 +6,6 @@ from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 
-
 SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
 API_URL = '/static/swagger.json'  # Our API url (can of course be a local resource)
 
